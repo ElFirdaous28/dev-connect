c7154190d1e121624462d9d777a6ddd0a5dbda9f
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-02-25 12:22:20 +0000

    DC-3 authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use Notifiable;
+
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +23,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'bio',
+        'gtihub_link',
+        'gitlab_link',
+        'profile_link',
     ];
 
     /**
