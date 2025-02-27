38093054be72fe8170b5dcc7e7d74097b4afde77
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-02-27 12:26:38 +0000

    DC-6 project managment

--- app/View/Components/suggested-connections.php
+++ app/View/Components/suggested-connections.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class suggested-connections extends Component
+class suggestedconnections extends Component
 {
     /**
      * Create a new component instance.
