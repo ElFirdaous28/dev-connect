3d9e56fe8ec121b277a5c681a70bd06a9b64fa99
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-03-20 13:54:25 +0000

    live coding

---
+++ app/Jobs/AutoPublishPost.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Post;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class AutoPublishPost implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $post;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $post = Post::find($this->post->id);
+
+        if ($post && $post->status === 'draft') {
+            $post->update(['status' => 'published']);
+        }
+    }
+}
